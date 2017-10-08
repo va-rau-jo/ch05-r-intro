4d2cb9b121d736d98bc3062ee934021c4ff39cd1
yan530 <yan530@uw.edu>
2017-10-07 18:11:03 -0700

    new exercise for lab

---
+++ exercise-2/exercise.R
@@ -0,0 +1,37 @@
+## exercise 2 -- if/else statement
+## now you now how to do the basic syntax in R, let's play with by using if/else statements
+## the game is called 'is it going to rain later in Seattle?'
+
+## make a variable called 'chance_of_rain' and assign it to be 0
+
+## assign 'true' or 'false' to a variable named 'I_saw_raindrops'
+
+## assign 'true' or 'false' to a variable named 'my_shoes_are_wet'
+
+## assign 'true' or 'false' to a variable named 'there_is_traffic'
+
+## assign 'true' or 'false' to a variable named 'my_ta_is_soaked'
+
+## assign 'true' or 'false' to a variable named 'I_love_cookies'
+
+## ok, now you have your variables ready to go.
+
+## make an if statement that checks whether you saw raindrops
+## if you did, chance of rain goes up by 40 percent
+
+## make an if statement that checks whether your shoes are wet
+## if you did, chance of rain goes up by 20 percent
+
+## make an if statement that checks whether there is traffic
+## if you did, chance of rain goes up by 10 percent
+
+## make an if statement that checks whether your TA is soaked
+## if you did, chance of rain goes up by 10 percent
+
+## make an if statement that checks whether you love cookies
+## if you don't, give a reason why not
+
+## Finally print out a sentence that says 'The chance of rain in Seattle is (the number stored in chance_of_rain variable) percent'
+
+## bonus:
+## how to make write this exercise in a function??
