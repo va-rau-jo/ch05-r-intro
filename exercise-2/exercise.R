cb2716d49223eda0cf554dc0543a56787e97ed35
yan530 <yan530@uw.edu>
2017-10-08 13:20:09 -0700

    changed answer

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,55 +1,44 @@
 ## exercise 2 -- if/else statement
-## now you now how to do the basic syntax in R, let's play with by using if/else statements
-## the game is called 'is it going to rain later in Seattle?'
+## now you know how to do the basic syntax in R, let's play with if/else statements
+## the game is called 'is it going to rain in Seattle?'
 
 ## make a variable called 'chance_of_rain' and assign it to be 0
 chance_of_rain <- 0
 
 ## assign 'true' or 'false' to a variable named 'I_saw_raindrops'
-I_saw_raindrops <- TRUE
+I_saw_raindrops <- FALSE
 
 ## assign 'true' or 'false' to a variable named 'my_shoes_are_wet'
 my_shoes_are_wet <- TRUE
-
-## assign 'true' or 'false' to a variable named 'there_is_traffic'
-there_is_traffic <- TRUE
 
 ## assign 'true' or 'false' to a variable named 'my_ta_is_soaked'
 my_ta_is_soaked <- TRUE
 
 ## assign 'true' or 'false' to a variable named 'I_love_cookies'
-I_love_cookies <- TRUE
+I_love_cookies <- FALSE
 
 ## ok, now you have your variables ready to go.
 
 ## make an if statement that checks whether you saw raindrops
-## if you did, chance of rain goes up by 40 percent
+## if you did, set chance_of_rain to 100
 if (I_saw_raindrops){
+  chance_of_rain <- 100
+  
+## else make an if statement that checks whether your shoes are wet
+## if they are, chance of rain goes up by 40 percent
+} else if (my_shoes_are_wet){
   chance_of_rain <- chance_of_rain + 40
-}
 
-## make an if statement that checks whether your shoes are wet
-## if you did, chance of rain goes up by 20 percent
-if (my_shoes_are_wet){
-  chance_of_rain <- chance_of_rain + 20
-}
-
-## make an if statement that checks whether there is traffic
-## if you did, chance of rain goes up by 10 percent
-if (there_is_traffic){
-  chance_of_rain <- chance_of_rain + 10
-}
-
-## make an if statement that checks whether your TA is soaked
-## if you did, chance of rain goes up by 10 percent
-if (my_ta_is_soaked){
-  chance_of_rain <- chance_of_rain + 10
+## else make an if statement that checks whether your TA is soaked
+## if you did, chance of rain goes up by 50 percent
+} else if (my_ta_is_soaked){
+  chance_of_rain <- chance_of_rain + 50
 }
 
 ## make an if statement that checks whether you love cookies
 ## if you don't, give a reason why not
 if (!I_love_cookies){
-  print <- ('I don\'t love cookies because I love brownies more!')
+  print('I don\'t love cookies because I love brownies more!')
 }
 
 ## Finally print out a sentence that says 'The chance of rain in Seattle is (the number stored in chance_of_rain variable) percent'
@@ -59,18 +48,16 @@ cat('The chance of rain in Seattle is', chance_of_rain, 'percent')
 ## how to make write this exercise in a function??
 ChanceOfRain <- function(I_saw_raindrops, my_shoes_are_wet, my_ta_is_soaked, I_love_cookies, chance_of_rain){
   if (I_saw_raindrops){
+    chance_of_rain <- 100
+  } else if (my_shoes_are_wet){
     chance_of_rain <- chance_of_rain + 40
-  }
-  if (my_shoes_are_wet){
-    chance_of_rain <- chance_of_rain + 20
-  }
-  if (my_ta_is_soaked){
-    chance_of_rain <- chance_of_rain + 10
+  } else if (my_ta_is_soaked){
+    chance_of_rain <- chance_of_rain + 50
   }
   if (!I_love_cookies){
-    print <- ('I don\'t love cookies because I love brownies more!')
+    print('I don\'t love cookies because I love brownies more!')
   }
   cat('The chance of rain in Seattle is', chance_of_rain, 'percent')
 }
 
-ChanceOfRain(TRUE, TRUE, TRUE, FALSE, 0)
+ChanceOfRain(FALSE, TRUE, TRUE, FALSE, 0)
